51cd01992c10734e05410494241fcf2b11e2196d
Ed <ed@polarity.io>
2019-03-17 20:34:56 -0400

    Minor updates to formatting

--- components/recorded-future-block.js
+++ components/recorded-future-block.js
@@ -1,57 +1,68 @@
 polarity.export = PolarityComponent.extend({
-    details: Ember.computed.alias('block.data.details'),
+  details: Ember.computed.alias('block.data.details'),
 
-    redThreat: '#fa5843',
-    greenThreat: '#7dd21b',
-    yellowThreat: '#ffc15d',
-    /**
-     * Radius of the ticScore circle
-     */
-    threatRadius: 15,
-    /**
-     * StrokeWidth of the ticScore circle
-     */
-    threatStrokeWidth: 2,
-    elementRadius: 20,
-    elementStrokeWidth: 4,
+  redThreat: '#fa5843',
+  greenThreat: '#7dd21b',
+  yellowThreat: '#ffc15d',
+  /**
+   * Radius of the ticScore circle
+   */
+  threatRadius: 15,
+  /**
+   * StrokeWidth of the ticScore circle
+   */
+  threatStrokeWidth: 2,
+  elementRadius: 20,
+  elementStrokeWidth: 4,
 
-    elementColor: Ember.computed('details.ticScore', function(){
-        return this._getThreatColor(this.get('details').risk.score);
-    }),
-    elementStrokeOffset: Ember.computed('details.ticScore', 'elementCircumference', function(){
-        return this._getStrokeOffset(this.get('details').risk.score, this.get('elementCircumference'));
-    }),
+  elementColor: Ember.computed('details.ticScore', function() {
+    return this._getThreatColor(this.get('details').risk.score);
+  }),
+  elementStrokeOffset: Ember.computed('details.ticScore', 'elementCircumference', function() {
+    return this._getStrokeOffset(this.get('details').risk.score, this.get('elementCircumference'));
+  }),
 
-    threatCircumference: Ember.computed('threatRadius', function () {
-        return 2 * Math.PI * this.get('threatRadius');
-    }),
-    elementCircumference: Ember.computed('elementCircumference', function(){
-        return 2 * Math.PI * this.get('elementRadius');
-    }),
-    _getStrokeOffset(ticScore, circumference){
-        let progress = ticScore / 100;
-        return circumference * (1 - progress);
-    },
-    _getThreatColor(ticScore){
-        if (ticScore >= 75) {
-            return this.get('redThreat');
-        } else if (ticScore >= 50) {
-            return this.get('yellowThreat');
-        } else {
-            return this.get('greenThreat');
-        }
-    },
+  threatCircumference: Ember.computed('threatRadius', function() {
+    return 2 * Math.PI * this.get('threatRadius');
+  }),
+  elementCircumference: Ember.computed('elementCircumference', function() {
+    return 2 * Math.PI * this.get('elementRadius');
+  }),
+  _getStrokeOffset(ticScore, circumference) {
+    let progress = ticScore / 100;
+    return circumference * (1 - progress);
+  },
+  _getThreatColor(ticScore) {
+    if (ticScore >= 75) {
+      return this.get('redThreat');
+    } else if (ticScore >= 50) {
+      return this.get('yellowThreat');
+    } else {
+      return this.get('greenThreat');
+    }
+  },
 
-    hasLocation: Ember.computed('block.data.details', function () {
-        let data = this.get('block.data.details');
-        return !!data.location;
-    }),
-    hasSighting: Ember.computed('block.data.details', function () {
-        let data = this.get('block.data.details');
-        return !!data.sightings;
-    }),
-    hasLink: Ember.computed('block.data.details', function () {
-        let data = this.get('block.data.details');
-        return !!data.intelCard;
-    })
+  hasLocation: Ember.computed('block.data.details', function() {
+    let details = this.get('block.data.details');
+    if (details.location && details.location.location) {
+      if (details.location.location.country) {
+        return true;
+      }
+      if (details.location.location.city) {
+        return true;
+      }
+      if (details.location.organization) {
+        return true;
+      }
+    }
+    return false;
+  }),
+  hasSighting: Ember.computed('block.data.details', function() {
+    let data = this.get('block.data.details');
+    return !!data.sightings;
+  }),
+  hasLink: Ember.computed('block.data.details', function() {
+    let data = this.get('block.data.details');
+    return !!data.intelCard;
+  })
 });
